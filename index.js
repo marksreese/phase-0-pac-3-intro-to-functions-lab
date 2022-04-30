f7e29b277cafd9c58fda4c50f777e82a9a0dd07c
Mark <marksreese@gmail.com>
2022-04-30 14:51:50 -0700

    commit tree 44c1cdbfb3dc705cde99eb676ce6639447d0c4b5

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+  function sayHiToHeadphonedRoommate(string){
+      switch (true){
+          case (string === string.toLowerCase()) :
+              return `I can't hear you!`;
+            case (string === string.toUpperCase()) :
+                return `YES INDEED!`;
+            default :
+                return `I would love to!`;
+      }
+  }
